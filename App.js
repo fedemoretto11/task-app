8ef12f52bc3e17936ddd2caf2718ea24e56fb660
fedemoretto11 <fedemoretto94@gmail.com>
2023-11-28 17:04:21 -0300

    Agragada fucnion: agregar tarea

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [textItem, setTextItem] = useState('');
+  const [itemList, setItemList] = useState([]);
+
+  const onChangeTextHandler = (text) => {
+    setTextItem(text)
+  }
+
+  const addItemToList = () => {
+    setItemList(prevList => [...prevList, {id: Math.random().toString(), value: textItem} 
+    ])  
+    setTextItem('')
+  }
+
+  const renderItemList = ({item}) => (
+    <View>
+      <Text>{item.value}</Text>
+    </View>
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder='Ingresar tarea' 
+          style={styles.textInput}
+          onChangeText={onChangeTextHandler}
+          value={textItem}
+        />
+        <Button 
+          title='Add' 
+          onPress={addItemToList}
+        />
+      </View>
+      <View>
+        <FlatList 
+          data={itemList}
+          renderItem={renderItemList}
+          keyExtractor={item => item.id}
+        />
+      </View>
     </View>
   );
 }
@@ -14,7 +51,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40,
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  textInput: {
+    width: 250,
+    borderBottomColor: '#f02354',
+    borderBottomWidth: 1,
+  }
 });
